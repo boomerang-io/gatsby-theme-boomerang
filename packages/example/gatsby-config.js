b3255fa80aa0a89e66528ee42181ec2094f59cb6
Timothy R. Bula <trbula@us.ibm.com>
2020-09-02 16:37:22 -0400

    chore: update things

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -27,20 +27,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    { resolve: "gatsby-theme-boomerang", options: {} },
-    // Override so not included
-    {
-      resolve: "@gatsby-contrib/gatsby-plugin-elasticlunr-search",
-      options: {
-        // Fields to index
-        fields: [],
-        // How to resolve each field`s value for a supported node type
-        resolvers: {
-          // For any node of type MarkdownRemark, list how to resolve the fields` values
-          MarkdownRemark: {},
-        },
-      },
-    },
-  ],
+  plugins: [{ resolve: "gatsby-theme-boomerang", options: { lunrSearch: true } }],
 };
