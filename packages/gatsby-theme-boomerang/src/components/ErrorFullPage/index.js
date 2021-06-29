14d6b772b87c7240db78da3e5233bc262ccc17bb
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2021-06-29 14:05:37 -0300

    release: 0.2.0-beta.16

--- packages/gatsby-theme-boomerang/src/components/ErrorFullPage/index.js
+++ packages/gatsby-theme-boomerang/src/components/ErrorFullPage/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { ErrorFullPage } from "@boomerang-io/carbon-addons-boomerang-react";
+// eslint-disable-next-line import/no-unresolved
 import { BASE_LAUNCH_ENV_URL } from "@gatsby-theme-boomerang/config/platformUrlConfig";
 import { container } from "./ErrorFullPage.module.scss";
 
 function ErrorFullPageComponent(props) {
   return (
     <div className={container}>
-      <ErrorFullPage statusUrl={`${BASE_LAUNCH_ENV_URL}/status`} {...props} />
+      <ErrorFullPage statusUrl={`${BASE_LAUNCH_ENV_URL}/support/status`} {...props} />
     </div>
   );
 }
