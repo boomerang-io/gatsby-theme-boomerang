31ab607bfed677a5c546c691c4b68327f94ab3f4
Benjamin Ruby <65971197+BenjaminRuby@users.noreply.github.com>
2021-09-16 11:29:55 -0600

    chore: remove console.log

--- packages/gatsby-theme-boomerang/src/hooks/useTracking.js
+++ packages/gatsby-theme-boomerang/src/hooks/useTracking.js
@@ -6,7 +6,6 @@ export default function useTracking(location) {
   console.log("use tracking");
   useEffect(() => {
     if (global.ibmStats) {
-      console.log("trigger page view");
       return ibmStats.pageview();
     }
   }, [location]);
