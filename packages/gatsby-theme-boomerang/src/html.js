5fbf5493812c517b328f952dbf62ca0ae9dcfc4a
Isabela Mayumi Kawabata Borges <isabela.kawabata@ibm.com>
2024-09-04 18:43:25 -0300

    fix: app and styles

--- packages/gatsby-theme-boomerang/src/html.js
+++ packages/gatsby-theme-boomerang/src/html.js
@@ -16,6 +16,7 @@ export default class HTML extends React.Component {
           {this.props.preBodyComponents}
           <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
+          <div id="app"></div>
           <div hidden>
             <span id="new-window-aria-desc-0">Opens in a new tab</span>
           </div>
